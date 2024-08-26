ae6b4450edc77de33dc1f6e88ac682da2bcbd663
devayanm <devayan9689@gmail.com>
2024-08-27 00:13:34 +0530

    minor fixes

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; 
-import vuetify from './plugins/vuetify'; 
-import store from './store'; 
-import NavigationButtons from './components/NavigationButtons.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router"; 
+import vuetify from "./plugins/vuetify"; 
+import store from "./store"; 
+import NavigationButtons from "./components/NavigationButtons.vue";
 
 const app = createApp(App);
 
-app.component('NavigationButtons', NavigationButtons);
+app.component("NavigationButtons", NavigationButtons);
 
 app.use(router); 
 app.use(vuetify); 
 app.use(store); 
-app.mount('#app');
+app.mount("#app");
